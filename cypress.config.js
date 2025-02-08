97c5773e181cd9854e612eb856c04d8f8499ac0c
Justin Wilkins <jwilkins2016@gmail.com>
2025-02-08 01:50:07 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,22 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'spec',
+    reporterOptions: {
+      output: 'cypress/results/test-results.json',
+    },
     baseUrl: 'https://justin-wilkins.netlify.app',
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
+      on('after:run', (results) => {
+        try {
+          const fs = require('fs');
+          fs.writeFileSync('cypress/results/test-results.json', JSON.stringify(results, null, 2));
+        } catch (error) {
+          console.error('Error writing results:', error);
+        }
+      });
     },
     video: false,
     screenshotOnRunFailure: true,
