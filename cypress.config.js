c7694d2283995bd355ec8d12112745622ec5dab6
Justin Wilkins <jwilkins2016@gmail.com>
2025-02-08 00:26:13 -0500

    Update cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://justin-wilkins.netlify.app',
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    video: false,
+    screenshotOnRunFailure: true,
+    viewportWidth: 1280,
+    viewportHeight: 720
   },
 });
