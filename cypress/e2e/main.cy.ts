27451d620ef40a6fc785e55b0a6ba052f7525819
Justin Wilkins <jwilkins2016@gmail.com>
2025-02-09 00:29:03 -0500

    Test changes

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -15,9 +15,39 @@ describe('Cypress Tests', () => {
         cy.get('tr').should('be.visible')
     });
 
+    it('Should remove a row on the dynamic table', () => {
+        cy.get('table tr').should('have.length', 1);
+        cy.get('input[name="name"]').type("Testing");
+        cy.get('button[type="submit"]').click();
+        cy.get('div[class="toast"]').should('be.visible');
+        cy.get('tr').should('be.visible');
+        cy.get('table tr').should('have.length', 2);
+        cy.get('.delete-row').click();
+        cy.get('table tr').should('have.length', 1);
+    });
+
     it('Should display accordion content when accordion header is clicked', () => {
         cy.get('button[class="accordion-header"]').contains("Item 1").click()
-        cy.get('div[class="accordion-content"]').should('be.visible').contains("This is the content of Item 1.")
+        cy.get('.accordion-content > p').should('be.visible')
+    });
+
+    it('Should have a width of 0% initially', () => {
+        cy.get('#progress-bar')
+            .should('have.css', 'width', '0px');
+    });
+
+
+    it('Should have a width greater than 0px after clicking "Update Progress"', () => {
+        cy.get('#update-progress').click();
+
+        cy.wait(500);
+
+        cy.get('#progress-bar')
+            .invoke('css', 'width')
+            .then((width) => {
+                const widthValue = parseFloat(width);
+                expect(widthValue).to.be.greaterThan(0);
+            });
     });
 
     it('Should fail', () => {
