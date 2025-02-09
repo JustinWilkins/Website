aa7366fbc98d1a71a1a9c4b47d0d0b9215a51021
Justin Wilkins <jwilkins2016@gmail.com>
2025-02-08 22:32:30 -0500

    F

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     reporter: "mochawesome",
     reporterOptions: {
-      reportDir: "results",
+      reportDir: "html-report",
       overwrite: true,
       html: false,
       json: true,
@@ -13,14 +13,6 @@ module.exports = defineConfig({
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      on('after:run', (results) => {
-        try {
-          const fs = require('fs');
-          fs.writeFileSync('results/test-results.json', JSON.stringify(results, null, 2));
-        } catch (error) {
-          console.error('Error writing results:', error);
-        }
-      });
     },
     video: false,
     screenshotOnRunFailure: true,
